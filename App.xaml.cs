bf32cbc8bd0f6013e1f7335fb86745ae646635ea
grehochka82 <lobodina0607@mail.ru>
2025-11-14 10:39:32 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Clicker_WPF_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Windows;
+
+namespace Clicker_WPF_
+{
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bytton_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            CounterText.Text = $"Счетчик:{counter}";
+        }
+    }
+}
